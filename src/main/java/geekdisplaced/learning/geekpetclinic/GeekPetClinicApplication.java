bda7f66c839a988176f1227ac7ab50039584f50b
GeekDisplaced <geek.displaced@icloud.com>
2021-08-15 10:44:42 +0100

    Spring Boot Project Initialzr & Custom .gitignore file

---
+++ src/main/java/geekdisplaced/learning/geekpetclinic/GeekPetClinicApplication.java
@@ -0,0 +1,13 @@
+package geekdisplaced.learning.geekpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeekPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/geekdisplaced/learning/geekpetclinic/GeekPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package geekdisplaced.learning.geekpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
