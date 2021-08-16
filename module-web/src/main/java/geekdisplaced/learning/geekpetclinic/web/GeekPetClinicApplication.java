9c91ab765a05c7286515be61942bc0a07c9762de
GeekDisplaced <geek.displaced@icloud.com>
2021-08-16 19:52:00 +0100

    Refactor to multi-module project, separate app from data (Closes #1)

--- module-web/src/main/java/geekdisplaced/learning/geekpetclinic/web/GeekPetClinicApplication.java
+++ module-web/src/main/java/geekdisplaced/learning/geekpetclinic/web/GeekPetClinicApplication.java
@@ -1,4 +1,4 @@
-package geekdisplaced.learning.geekpetclinic;
+package geekdisplaced.learning.geekpetclinic.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- module-web/src/test/java/geekdisplaced/learning/geekpetclinic/web/GeekPetClinicApplicationTests.java
+++ module-web/src/test/java/geekdisplaced/learning/geekpetclinic/web/GeekPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package geekdisplaced.learning.geekpetclinic;
+package geekdisplaced.learning.geekpetclinic.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
